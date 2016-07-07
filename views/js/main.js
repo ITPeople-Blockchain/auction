df2752760a8229641b358b12d012c6f4b1e3825f
ratnakar <asara_ratnakar@yahoo.co.in>
2016-07-07 00:56:21 -0400

    Timer fix and other related fixes for Net mode

--- views/js/main.js
+++ views/js/main.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
+  //TODO: How to disable localstorage for next session
+  // Enable this for firsttime, and disable for subsequent runs
+  //localStorage.setItem("chaincodeHash", '');
   mainApp.init();
+
 });
